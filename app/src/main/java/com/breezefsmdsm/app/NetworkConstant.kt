2656c17edfd30cf132a44a3c524f1713cd1c30e0
SumanBacharIndusNet <suman.bachar@indusnet.co.in>
2022-10-08 08:30:37 +0530

    c

--- app/src/main/java/com/breezefsmdsm/app/NetworkConstant.kt
+++ app/src/main/java/com/breezefsmdsm/app/NetworkConstant.kt
@@ -28,8 +28,8 @@ class NetworkConstant {
         //var BASE_URL = "http://fts.indusnettechnologies.com:8094/API/"
         //var ADD_SHOP_BASE_URL = "http://fts.indusnettechnologies.com:8094/"
 
-        var BASE_URL = "http://fts.indusnettechnologies.com:7007/API/"
-        var ADD_SHOP_BASE_URL = "http://fts.indusnettechnologies.com:7007/"
+        var BASE_URL = "http://ftsv2.indusnettechnologies.com:7001/API/"
+        var ADD_SHOP_BASE_URL = "http://ftsv2.indusnettechnologies.com:7001/"
 
 
         //var BASE_URL = "http://3.7.30.86:82/API/"  //  local
